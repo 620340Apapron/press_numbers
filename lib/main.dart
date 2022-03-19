ffbc37724de2e3d5e20e23a5a943469310d024cc
Apapron Pipatpanuwittaya <meanee3003@gmail.com>
2022-03-19 15:34:47 +0700

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,193 +1,22 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:press_numbers/pages/home/home_page.dart';
 
 void main() {
-  const app = MyApp();
-  runApp(app);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Flutter Food',
       theme: ThemeData(
-        primarySwatch: Colors.grey,
+        primarySwatch: Colors.amber,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  static const buttonSize = 70.0;
-
-  MyHomePage({Key? key}) : super(key: key) {}
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _input = '';
-  var _num = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey.shade50,
-            borderRadius: BorderRadius.circular(16.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.shade100,
-                offset: Offset(5.0, 5.0),
-                spreadRadius: 2.0,
-                blurRadius: 5.0,
-              )
-            ],
-          ),
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(width: 8.0),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.lock,
-                          size: 80,
-                          color: Colors.black54,
-                        ),
-                        Text('กรุณาใส่รหัสผ่าน',
-                            style: TextStyle(
-                                fontSize: 36.0, color: Colors.black54)),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 50.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    for (var i = 0; i < 6; i++)
-                      Container(
-                        width: 20.0,
-                        height: 20.0,
-                        margin: EdgeInsets.all(4.0),
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                  ],
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  buildButton(1),
-                  buildButton(2),
-                  buildButton(3),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  buildButton(4),
-                  buildButton(5),
-                  buildButton(6),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  buildButton(7),
-                  buildButton(8),
-                  buildButton(9),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: MyHomePage.buttonSize,
-                      height: MyHomePage.buttonSize,
-                    ),
-                  ),
-                  buildButton(0),
-                  buildButton(-1),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildButton(int num) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: InkWell(
-        onTap: () {
-          if (num == -1) {
-            print('Backspace');
-            setState(() {
-              var length = _input.length;
-              _input = _input.substring(0, length - 1);
-            });
-          } else {
-            print('$num');
-            // Row(
-            //   mainAxisAlignment: MainAxisAlignment.center,
-            //     for (var i = 0; i < _num; i++)
-            //       Container(
-            //         width: 20.0,
-            //         height: 20.0,
-            //         margin: EdgeInsets.all(4.0),
-            //         decoration: BoxDecoration(
-            //           color: Colors.black54,
-            //           shape: BoxShape.circle,
-            //         ),
-            //       ),
-            //   ],
-            // );
-          }
-        },
-        borderRadius: BorderRadius.circular(MyHomePage.buttonSize / 2),
-        child: Container(
-          decoration: (num == -1)
-              ? null
-              : BoxDecoration(
-                  border: Border.all(width: 2.0),
-                  shape: BoxShape.circle,
-                ),
-          alignment: Alignment.center,
-          width: MyHomePage.buttonSize,
-          height: MyHomePage.buttonSize,
-          child: (num == -1)
-              ? const Icon(Icons.backspace)
-              : Text('$num',
-                  style: TextStyle(fontSize: 36.0, color: Colors.black54)),
-        ),
-      ),
+      home: const HomePage(),
     );
   }
 }
--- lib/test_page.dart
+++ lib/test_page.dart
@@ -1,34 +1,241 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:press_numbers/pages/home/home_page.dart';
 
-class TestPage extends StatefulWidget {
-  const TestPage({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  static const routeName = '/login';
+
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  _TestPageState createState() => _TestPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _TestPageState extends State<TestPage> {
-  var _num = 0;
+class _LoginPageState extends State<LoginPage> {
+  static const PIN = '123456';
+  var _input = '123';
+  var _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('MAIN PAGE')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    _num++;
-                  });
-                },
-                child: Text('TEST')),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.white,
+                  Theme.of(context).colorScheme.background.withOpacity(0.5),
+
+                ],
+              ),
+            ),
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.lock_outline,
+                              size: 90.0,
+                              color:
+                              Theme.of(context).textTheme.headline1?.color,
+                            ),
+                            Text(
+                              'LOGIN',
+                              style: Theme.of(context).textTheme.headline1,
+                            ),
+                            SizedBox(height: 6.0),
+                            Text(
+                              'Enter PIN to login',
+                              style: Theme.of(context).textTheme.bodyText2,
+                            )
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            for (var i = 0; i < _input.length; i++)
+                              Container(
+                                margin: EdgeInsets.all(4.0),
+                                width: 24.0,
+                                height: 24.0,
+                                decoration: BoxDecoration(
+                                    color:
+                                    Theme.of(context).colorScheme.primary,
+                                    shape: BoxShape.circle),
+                              ),
+                            for (var i = _input.length; i < 6; i++)
+                              Container(
+                                margin: EdgeInsets.all(4.0),
+                                width: 24.0,
+                                height: 24.0,
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .primary
+                                      .withOpacity(0.2),
+                                  shape: BoxShape.circle,
+                                ),
+                              ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  _buildNumPad(context),
+                ],
+              ),
+            ),
           ),
+          if (_isLoading)
+            Container(
+              color: Colors.black.withOpacity(0.5),
+              child: Center(
+                child: SizedBox(
+                  width: 20.0,
+                  height: 20.0,
+                  child: CircularProgressIndicator(color: Colors.white),
+                ),
+              ),
+            ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildNumPad(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          [1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+          [-2, 0, -1],
+        ].map((row) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: row.map((item) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: LoginButton(
+                  number: item,
+                  onClick: () {
+                    _handleClickButton(context, item);
+                  },
+                ),
+              );
+            }).toList(),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  _handleClickButton(BuildContext context, int num) async {
+    print('You pressed $num');
+
+    if (num == -1) {
+      if (_input.length > 0) {
+        setState(() {
+          _input = _input.substring(0, _input.length - 1);
+        });
+      }
+    } else if (_input.length < PIN.length) {
+      setState(() {
+        _input = '$_input$num';
+      });
+    }
+
+    if (_input.length == PIN.length) {
+      if (_input == PIN) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const MainPage()),
+        );
+      } else {
+        _showOkDialog(context, 'Incorrect PIN', 'Please try again');
+        setState(() {
+          _input = '';
+        });
+      }
+
+    }
+  }
+
+  void _showOkDialog(BuildContext context, String title, String content) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(content),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<bool?> _login(String pin) async {}
+}
+
+class LoginButton extends StatelessWidget {
+  final int number;
+  final Function() onClick;
+
+  const LoginButton({
+    required this.number,
+    required this.onClick,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      customBorder: CircleBorder(),
+      onTap: number == -2 ? null : onClick,
+      child: Container(
+        width: 75.0,
+        height: 75.0,
+        decoration: number < 0
+            ? null
+            : BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white.withOpacity(0.5),
+          border: Border.all(
+            width: 3.0,
+            color: Theme.of(context).textTheme.headline1!.color!,
+          ),
+        ),
+        child: Center(
+          child: number >= 0
+              ? Text(
+            '$number', // number.toString()
+            style: Theme.of(context).textTheme.headline6,
+          )
+              : (number == -1
+              ? Icon(
+            Icons.backspace_outlined,
+            size: 28.0,
+          )
+              : SizedBox.shrink()),
+        ),
       ),
     );
   }
